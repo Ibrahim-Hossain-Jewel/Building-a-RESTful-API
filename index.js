ba8755b0fd89aec07d2c1baf5ec678deecc931c0
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-01-14 01:52:38 +0600

    parsing request path

--- index.js
+++ index.js
@@ -5,8 +5,17 @@
 */
 // Dependencies
 const http = require('http');
+const url = require('url');
+
 const server = http.createServer((req,res  )=>{
-    res.end("Welcome jewel server created successfully!");
+    //Get the url and parse it.
+    const parseUrl = url.parse(req.url,true);
+    //Get the pathname from parseUrl.
+    const path = parseUrl.pathname;
+    //Get the trimmed name here.
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    res.end("Your client pathname is : " + trimmedPath );
 });
 server.listen("3000",()=>{
     console.log("your server is working property now run your server execution!");
